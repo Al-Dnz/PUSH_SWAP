40f521ca86ae87e2436aab8c2849c2232dc5017b
AlexDNZ <paleozene@gmail.com>
2021-06-15 13:42:45 +0200

    fix reverse signal

--- assets/src/register_util_2.c
+++ assets/src/register_util_2.c
@@ -6,7 +6,7 @@
 /*   By: adenhez <adenhez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 13:22:04 by adenhez           #+#    #+#             */
-/*   Updated: 2021/06/15 13:22:05 by adenhez          ###   ########.fr       */
+/*   Updated: 2021/06/15 13:40:33 by adenhez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ t_reg	*register_last(t_reg *lst)
 int	read_seq(t_reg *reg)
 {
 	int			n;
-	static char	*arr[12] = {"", "sa", "sb", "ss", "pa", "pb", "ra", "rb",
-"rr", "rra", "rrb", "rrr"};
+	static char	*arr[12] = {"", "sa", "sb", "ss", "pb", "pa", "rra", "rrb",
+"rrr", "ra", "rb", "rr"};
 
 	n = 0;
 	while (reg != NULL)
