4554db03a4d48cca5977eda3b9652e5e3b2b6390
AlexDNZ <paleozene@gmail.com>
2021-06-15 13:51:36 +0200

    add tester from 42 fix checker signal

--- assets/src/switch_checker.c
+++ assets/src/switch_checker.c
@@ -6,7 +6,7 @@
 /*   By: adenhez <adenhez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/30 13:47:27 by adenhez           #+#    #+#             */
-/*   Updated: 2021/06/09 16:36:31 by adenhez          ###   ########.fr       */
+/*   Updated: 2021/06/15 13:43:19 by adenhez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,28 +27,28 @@ void	swapper(char *str, t_list **li_a, t_list **li_b, t_state *state)
 
 void	pusher(char *str, t_list **li_a, t_list **li_b, t_state *state)
 {
-	if (ft_strcmp(str, "pa") == 0)
-		transfer_top(li_a, li_b, 0, state);
 	if (ft_strcmp(str, "pb") == 0)
+		transfer_top(li_a, li_b, 0, state);
+	if (ft_strcmp(str, "pa") == 0)
 		transfer_top(li_b, li_a, 0, state);
 }
 
 void	rotater(char *str, t_list **li_a, t_list **li_b, t_state *state)
 {
-	if (ft_strcmp(str, "ra") == 0)
+	if (ft_strcmp(str, "rra") == 0)
 		shift_up(li_a, 0, state);
-	if (ft_strcmp(str, "rb") == 0)
+	if (ft_strcmp(str, "rrb") == 0)
 		shift_up(li_b, 0, state);
-	if (ft_strcmp(str, "rr") == 0)
+	if (ft_strcmp(str, "rrr") == 0)
 	{
 		shift_up(li_a, 0, state);
 		shift_up(li_b, 0, state);
 	}
-	if (ft_strcmp(str, "rra") == 0)
+	if (ft_strcmp(str, "ra") == 0)
 		shift_down(li_a, 0, state);
-	if (ft_strcmp(str, "rrb") == 0)
+	if (ft_strcmp(str, "rb") == 0)
 		shift_down(li_b, 0, state);
-	if (ft_strcmp(str, "rrr") == 0)
+	if (ft_strcmp(str, "rr") == 0)
 	{
 		shift_down(li_a, 0, state);
 		shift_down(li_b, 0, state);
